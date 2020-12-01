ec607214e67f3411ef6590c0653041b5a1e276bb
Ashwani Dubey <ashwani786dubey@gmail.com>
2020-12-01 15:46:57 +0530

    database of question paper are added now

--- student_performance_analysis/user_app/admin.py
+++ student_performance_analysis/user_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import QuestionBank
 # Register your models here.
+admin.site.register(QuestionBank)
--- student_performance_analysis/user_app/models.py
+++ student_performance_analysis/user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class QuestionBank(models.Model):
+    q_id=models.AutoField(primary_key=True)
+    question=models.CharField(max_length=1000)
+    opA=models.CharField(max_length=100)
+    opB=models.CharField(max_length=100)
+    opC=models.CharField(max_length=100)
+    opD=models.CharField(max_length=100)
+    answer=models.CharField(max_length=100)
+    def __str__(self):
+        return str(self.q_id)
