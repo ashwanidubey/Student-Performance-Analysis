4649f73a392d0b2396880b28fc25113bca5dc5d4
Ashwani Dubey <ashwani786dubey@gmail.com>
2020-12-01 13:31:23 +0530

    name changed

--- student_performance_analysis/student_performance_analysis/wsgi.py
+++ student_performance_analysis/student_performance_analysis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'performance_analysis_through_datamining.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_performance_analysis.settings')
 
 application = get_wsgi_application()
