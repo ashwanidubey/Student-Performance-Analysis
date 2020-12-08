15df423ba9bb54fa29d7a5e9ffa31f196a98061d
Ashwani Dubey <ashwani786dubey@gmail.com>
2020-12-08 20:13:50 +0530

    linking home page with the form and take test

--- student_performance_analysis/user_app/urls.py
+++ student_performance_analysis/user_app/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 
 urlpatterns=[
 url('home/',views.home),
+url('myform/',views.myform),
+url('taketest/',views.taketest),
 #url('^$',views.home),
 
 ]
--- student_performance_analysis/user_app/views.py
+++ student_performance_analysis/user_app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'user_app/index.html')
+def myform(request):
+    return render(request,'user_app/myform.html')
+def taketest(request):
+        return render(request,'user_app/taketest.html')
