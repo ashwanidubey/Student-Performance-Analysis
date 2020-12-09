f6d7c7d6d7e7a2020c7e77f72bd4563dfa9056fa
Ashwani Dubey <ashwani786dubey@gmail.com>
2020-12-09 18:00:13 +0530

    addingg predict page and show questions

--- student_performance_analysis/user_app/urls.py
+++ student_performance_analysis/user_app/urls.py
@@ -5,6 +5,10 @@ urlpatterns=[
 url('home/',views.home),
 url('myform/',views.myform),
 url('taketest/',views.taketest),
+url('predict/',views.predict),
+url('history/',views.history),
+url('help/',views.help),
+#url('predict/',views.predict),
 #url('^$',views.home),
 
 ]
--- student_performance_analysis/user_app/views.py
+++ student_performance_analysis/user_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . import  models
 
 
 
@@ -8,4 +8,16 @@ def home(request):
 def myform(request):
     return render(request,'user_app/myform.html')
 def taketest(request):
-        return render(request,'user_app/taketest.html')
+    q=models.QuestionBank.objects.all()
+    return render(request,'user_app/taketest.html',{'q':q})
+def predict(request):
+    if request.method=='POST':
+        print(request.POST)
+        data={'name':request.POST['yourname'],'email':request.POST['youremail'],'cpi':request.POST['yourpercent13'],'branch':request.POST['yourbranch'],'gender':request.POST['yourgender'],'inter':request.POST['yourpercent12'],'highschool':request.POST['yourpercent10'],'currentback':request.POST['yourcb'],'everback':request.POST['youreb'],'project':request.POST['yourproject'],'techevent':request.POST['yourte'],'techquiz':request.POST['yourtq']}
+        for x in data:
+            print(x," ",data[x])
+        return render(request,'user_app/predict.html',data)
+def history(request):
+    return render(request,'user_app/history.html')
+def help(request):
+    return render(request,'user_app/help.html')
