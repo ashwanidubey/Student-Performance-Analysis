cbfbc92d57bab4aae4fb11827203c68d3030f1d3
Deepika Pathak <deepikapathak228750@gmail.com>
2020-12-31 19:22:51 +0530

    modified form and taketest

--- student_performance_analysis/user_app/views.py
+++ student_performance_analysis/user_app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from . import  models
 import random
-
+q_nos=list()
 
 def home(request):
     return render(request,'user_app/index.html')
 def myform(request):
     return render(request,'user_app/myform.html')
 def taketest(request):
+    global q_nos
+    q_nos.clear()
     randlist=random.sample(range(1,40),10)
     randlist.sort()
-
-    return render(request,'user_app/taketest.html')
+    for i in range(0,10):
+        q_nos.append(randlist[i])
+    q=models.QuestionBank.objects.filter(q_id=q_nos[0])
+    return render(request,'user_app/taketest.html',{'q':q})
 def predict(request):
     if request.method=='POST':
         print(request.POST)
@@ -23,3 +27,6 @@ def history(request):
     return render(request,'user_app/history.html')
 def help(request):
     return render(request,'user_app/help.html')
+def showquestion(request):
+      q=models.QuestionBank.objects.filter(q_id=q_nos[1])
+      return render(request,'user_app/taketest.html',{'q':q})
