b86c081fc6923ea015d411c3dda93f797157720b
Ashwani Dubey <ashwani786dubey@gmail.com>
2021-04-14 11:33:58 +0530

    remainh_models are created

--- student_performance_analysis/user_app/admin.py
+++ student_performance_analysis/user_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Aptitude,Communication,ProgrammingLogic
+from .models import Aptitude,Communication,ProgrammingLogic,Management,Reasoning
 # Register your models here.
 admin.site.register(Aptitude)
 admin.site.register(Communication)
 admin.site.register(ProgrammingLogic)
+admin.site.register(Reasoning)
+admin.site.register(Management)
--- student_performance_analysis/user_app/models.py
+++ student_performance_analysis/user_app/models.py
@@ -30,3 +30,26 @@ class ProgrammingLogic(models.Model):
     answer=models.CharField(max_length=100)
     def __str__(self):
         return "question no : "+str(self.q_id)
+class Reasoning(models.Model):
+    q_id=models.AutoField(primary_key=True)
+    question=models.CharField(max_length=1000)
+    opA=models.CharField(max_length=100)
+    opB=models.CharField(max_length=100)
+    opC=models.CharField(max_length=100)
+    opD=models.CharField(max_length=100)
+    answer=models.CharField(max_length=100)
+    def __str__(self):
+        return "question no : "+str(self.q_id)
+class Management(models.Model):
+    q_id=models.AutoField(primary_key=True)
+    question=models.CharField(max_length=1000)
+    opA=models.CharField(max_length=100)
+    opB=models.CharField(max_length=100)
+    opC=models.CharField(max_length=100)
+    opD=models.CharField(max_length=100)
+    answerA=models.CharField(max_length=100)
+    answerB=models.CharField(max_length=100)
+    answerC=models.CharField(max_length=100)
+    answerD=models.CharField(max_length=100)
+    def __str__(self):
+        return "question no : "+str(self.q_id)
