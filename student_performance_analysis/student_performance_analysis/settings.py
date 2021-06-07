049c3b4173a9f12ceffd41a40bd82f2477c89777
Ashwani Dubey <ashwani786dubey@gmail.com>
2021-06-07 19:31:15 +0530

    hosting

--- student_performance_analysis/student_performance_analysis/settings.py
+++ student_performance_analysis/student_performance_analysis/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!q6t_=h(p9e*kmfb$sy_%4^*zm%q%$pr3uhrg#)%o%by8=%x0s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
